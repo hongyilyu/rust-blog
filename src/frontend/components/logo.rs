88efe10f1bf2a08e03ea897095c24dc7674aaa29
Hongyi Lyu <hongyi.lyu95@gmail.com>
2024-02-05 23:04:22 -0800

    fix local storage issue

--- src/frontend/components/logo.rs
+++ src/frontend/components/logo.rs
@@ -1,10 +1,17 @@
 use leptos::*;
+use leptos_use::{storage::use_local_storage, utils::FromToStringCodec};
+
+use crate::frontend::components::theme_control::Theme;
 
 #[component]
 pub fn Logo() -> impl IntoView {
+    let (theme, _, _) = use_local_storage::<Theme, FromToStringCodec>("theme");
     view! {
         <img
-            src="/logo-black-bg.svg"
+            src=move || match theme.get() {
+                Theme::Dark => "/logo-black-bg.svg",
+                Theme::Light => "/logo-white-bg.svg",
+            }
             alt="Hongyi Lyu"
             viewBox="0 0 148 144"
             width="32px"
